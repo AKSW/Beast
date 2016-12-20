89136a9bea8eb53c0806d0ef6d184faa002d3dd6
Claus Stadler <RavenArkadon@googlemail.com>
2016-12-20 20:09:51 +0100

    Added aggregation utility

--- beast-examples/src/main/java/org/aksw/beast/examples/Multithreaded.java
+++ beast-examples/src/main/java/org/aksw/beast/examples/Multithreaded.java
@@ -3,19 +3,23 @@ package org.aksw.beast.examples;
 import java.util.List;
 import java.util.Random;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import org.aksw.iguana.vocab.IguanaVocab;
 import org.aksw.jena_sparql_api.rdf_stream.core.RdfStream;
 import org.aksw.jena_sparql_api.rdf_stream.enhanced.ResourceEnh;
 import org.aksw.jena_sparql_api.rdf_stream.processors.PerformanceAnalyzer;
 import org.aksw.jena_sparql_api.vocabs.IV;
+import org.aksw.jena_sparql_api.vocabs.OWLTIME;
 import org.aksw.simba.lsq.vocab.LSQ;
 import org.apache.jena.query.Query;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.sparql.expr.aggregate.AggAvg;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -51,8 +55,17 @@ public class Multithreaded {
 					.peek(r -> r.addLiteral(IV.thread, i + 1))
 					.map(r -> r.rename("http://ex.org/thread{0}-run{1}-query{2}", IV.thread, IV.run, IV.job)));
 
-		WorkflowExecutor.join(workflowGen)
-			.forEach(r -> RDFDataMgr.write(System.out, r.getModel(), RDFFormat.TURTLE_BLOCKS));
+		List<Resource> observations = WorkflowExecutor.join(workflowGen)
+			.collect(Collectors.toList());
+			//.forEach(r -> RDFDataMgr.write(System.out, r.getModel(), RDFFormat.TURTLE_BLOCKS));
+
+
+		Grouper.enh()
+			.on(IV.run)
+			.on(IguanaVocab.workload)
+			.agg(IV.experiment, OWLTIME.numericDuration, AggAvg.class)
+			.apply(observations.stream())
+		.forEach(r -> RDFDataMgr.write(System.out, r.getModel(), RDFFormat.TURTLE_BLOCKS));
 
 	}
 
