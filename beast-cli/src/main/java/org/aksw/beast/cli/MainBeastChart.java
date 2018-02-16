0d7ba1309693e031dd9cea35b1f7c4f5ef38a49b
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2018-02-16 10:50:49 +0100

    wip

--- beast-cli/src/main/java/org/aksw/beast/cli/MainBeastChart.java
+++ beast-cli/src/main/java/org/aksw/beast/cli/MainBeastChart.java
@@ -1,6 +1,6 @@
 package org.aksw.beast.cli;
 
-import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Objects;
@@ -11,8 +11,12 @@ import org.aksw.beast.viz.xchart.ChartModelConfigurerXChart;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.RDFDataMgr;
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.BitmapEncoder.BitmapFormat;
 import org.knowm.xchart.CategoryChart;
 import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.VectorGraphicsEncoder;
+import org.knowm.xchart.VectorGraphicsEncoder.VectorGraphicsFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,7 +31,10 @@ public class MainBeastChart {
 
     protected OptionParser parser = new OptionParser();
 
-    protected OptionSpec<File> inputOs;
+    protected OptionSpec<?> svgOs;
+    protected OptionSpec<?> pngOs;    
+    protected OptionSpec<?> guiOs;    
+    
 //    protected OptionSpec<File> outputOs;
 //    protected OptionSpec<String> logFormatOs;
 //    protected OptionSpec<String> outFormatOs;
@@ -35,18 +42,10 @@ public class MainBeastChart {
 
     public MainBeastChart() {
     	parser.allowsUnrecognizedOptions();
-    	
-    
-    	
-		// TODO Allow as non-option argument
-//    	inputOs = parser
-//                .acceptsAll(Arrays.asList("f", "file"), "File containing input data")
-//                .withRequiredArg()
-//                .ofType(File.class)
-//                ;
-
-    
-    
+    	    
+    	pngOs = parser.acceptsAll(Arrays.asList("png"), "Output charts in png format (Default if no other format is given)");
+    	svgOs = parser.acceptsAll(Arrays.asList("svg"), "Output charts in svg format");
+    	guiOs = parser.acceptsAll(Arrays.asList("gui"), "Display charts in a window");
     }
     
     public void run(String[] args) throws Exception {
@@ -54,6 +53,10 @@ public class MainBeastChart {
     	
     	List<?> inputResources = options.nonOptionArguments();
 
+    	boolean isSvgOutputEnabled = options.has(svgOs);
+    	boolean isPngOutputEnabled = options.has(pngOs);
+    	boolean isGuiOutputEnabled = options.has(guiOs);
+    	
     	Model model = ModelFactory.createDefaultModel();
     	for(Object inputResource : inputResources) {
     		RDFDataMgr.read(model, Objects.toString(inputResource));
@@ -65,7 +68,19 @@ public class MainBeastChart {
     	for(Entry<StatisticalBarChart, Model> chartSpec : chartSpecs) {
             CategoryChart xChart = ChartModelConfigurerXChart.toChart(chartSpec.getValue(), chartSpec.getKey());
 
-            new SwingWrapper<CategoryChart>(xChart).displayChart();
+            VectorGraphicsEncoder.saveVectorGraphic(xChart, "/tmp/Sample_Chart", VectorGraphicsFormat.SVG);
+            
+            if(isSvgOutputEnabled) {
+            	BitmapEncoder.saveBitmap(xChart, "/tmp/Sample_Chart", BitmapFormat.PNG);
+            }
+            
+            if(isPngOutputEnabled) {
+            	BitmapEncoder.saveBitmap(xChart, "/tmp/Sample_Chart", BitmapFormat.JPG);
+            }
+            
+            if(isGuiOutputEnabled) {
+                new SwingWrapper<CategoryChart>(xChart).displayChart();
+            }            
     	}
     	
     }
